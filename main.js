0e75847126f8c26780f0c61bb1152eeaf8322451
jeongbaebang <jeongbaebnag_dev@naver.com>
2020-11-21 17:07:49 +0900

    Project

--- main.js
+++ main.js
@@ -8,6 +8,19 @@
     ioIndex = entries[0].target.dataset.index * 1; //타입을 숫자로 만들기 위해 * 1 처리.
   });
 
+  const actions = {
+    birdFlies(bool) {
+      if (bool) {
+        document.querySelector(
+          '[data-index="2"] .bird'
+        ).style.transform = `translateX(${window.innerWidth}px)`;
+      } else {
+        document.querySelector(
+          '[data-index="2"] .bird'
+        ).style.transform = `translateX(-100%)`;
+      }
+    },
+  };
   function setDataCirculation(nodeArray) {
     nodeArray.forEach((node) => {
       node.forEach((element, key) => {
@@ -22,36 +35,44 @@
 
   setDataCirculation([$stepElems, $graphicElems]);
 
-  function activate() {
+  function activate(action) {
     currentItem.classList.add("visible");
+    if (action) {
+      actions[action](true);
+    }
   }
 
-  function inactivate() {
+  function inactivate(action) {
     currentItem.classList.remove("visible");
+    if (action) {
+      actions[action](false);
+    }
   }
 
   window.addEventListener("scroll", () => {
     let boundingRect = null;
     let step = null;
-    let temp = null;
 
     for (let i = ioIndex - 1; i < ioIndex + 2; i++) {
       step = $stepElems[i];
       if (!step) continue;
       boundingRect = step.getBoundingClientRect();
-      temp += 1;
 
       if (
         boundingRect.top > window.innerHeight * 0.1 &&
         boundingRect.top < window.innerHeight * 0.8
       ) {
         if (currentItem) {
-          inactivate();
+          inactivate(currentItem.dataset.action);
         }
         currentItem = $graphicElems[step.dataset.index];
-        activate();
+        activate(currentItem.dataset.action);
       }
     }
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => scrollTo(0, 0), 100);
   });
   activate();
 })();
